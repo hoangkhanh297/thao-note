bb2c93aeab37fe202ef66906f080242925ea197b
Khanh Hoang <hoangkhanh297@gmail.com>
2021-10-12 17:35:43 +0700

    Update avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,10 @@ import {
   View,
   Image,
   Text,
-  TouchableOpacity,
   Modal,
-  Pressable
+  Pressable,
+  TextInput,
+  LogBox
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import * as ImagePicker from "react-native-image-picker"
@@ -17,14 +18,60 @@ const COLOR = {
   greenColor: 'rgb(77, 196, 144)',
   pinkColor: '#CF9EF5',
 };
+
 const Header = () => {
   const [resourcePath, setResourcePath] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [isChangeName, setIsChangeName] = useState(false);
+  const [appName, setAppName] = useState('Thảo Nguyễn Todo App');
+  const [tempAppName, setTempAppName] = useState('Thảo Nguyễn Todo App');
+  const [imgHeight, setImgHeight] = useState(400);
+  const [imgWidth, setImgWidth] = useState(400);
   let actionSheet = useRef();
+
+  const Popup = () => {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        animationType='fade'
+        visible={modalVisible}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Image
+              source={
+                resourcePath === undefined
+                  || resourcePath === null
+                  || resourcePath.assets[0] === undefined
+                  || resourcePath.assets[0] === null
+                  || resourcePath.assets[0].uri == undefined
+                  ? require('../assets/images/avt.jpg')
+                  : { uri: resourcePath.assets[0].uri }}
+              style={{ height: imgHeight, width: imgWidth, }}
+            />
+            <View style={{ flexDirection: 'row', marginTop: 15, width: 200, justifyContent: 'space-between' }}>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>OK</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => selectFile()}
+              >
+                <Text style={styles.textStyle}>Đổi ảnh</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
   var optionArray = [
     'Xem ảnh',
     'Đổi ảnh',
-    'Đổi tên',
     'Huỷ'
   ];
 
@@ -34,9 +81,23 @@ const Header = () => {
   };
 
   useEffect(() => {
-    console.log('Test user effect')
     loadImage();
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
   }, []);
+
+  useEffect(() => {
+    saveImage();
+    if (resourcePath === undefined
+      || resourcePath === null
+      || resourcePath.assets[0] === undefined
+      || resourcePath.assets[0] === null
+      || resourcePath.assets[0].uri == undefined) {
+      console.log('Do not do anything');
+    } else {
+      setImgHeight(Math.min(resourcePath.assets[0].height / 3, imgHeight))
+      setImgWidth(Math.min(resourcePath.assets[0].width / 3, imgWidth))
+    }
+  }, [resourcePath]);
   async function loadImage() {
     try {
       const value = await AsyncStorage.getItem('@storage_Key_Image');
@@ -66,8 +127,6 @@ const Header = () => {
       mediaType: 'photo',
       saveToPhotos: true,
       includeBase64: true,
-      maxWidth: 200,
-      maxHeight: 200,
       quality: 1,
     };
     ImagePicker.launchImageLibrary(options, res => {
@@ -78,14 +137,14 @@ const Header = () => {
       } else {
         console.log('Response = ', res);
         setResourcePath(res)
-        saveImage()
       }
     });
   };
   return (
+
     <View style={styles.headerContainer}>
       <View style={styles.avatarContainer}>
-        <TouchableOpacity
+        <Pressable
           onPress={showActionSheet}
         >
           <Image
@@ -99,58 +158,30 @@ const Header = () => {
                 : { uri: resourcePath.assets[0].uri }}
             style={styles.avatarImage}
           />
-        </TouchableOpacity>
-        {modalVisible ? (
-          <Modal
-            animationType="slide"
-            transparent={true}
-            animationType='fade'
-            visible={modalVisible}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <Image
-                  source={
-                    resourcePath === undefined
-                      || resourcePath === null
-                      || resourcePath.assets[0] === undefined
-                      || resourcePath.assets[0] === null
-                      || resourcePath.assets[0].uri == undefined
-                      ? require('../assets/images/avt.jpg')
-                      : { uri: resourcePath.assets[0].uri }}
-                  style={styles.avatarImage}
-                />
-                <Pressable
-                  style={[styles.button, styles.buttonClose]}
-                  onPress={() => setModalVisible(!modalVisible)}
-                >
-                  <Text style={styles.textStyle}>Đóng</Text>
-                </Pressable>
-              </View>
-            </View>
-          </Modal>) : (<View />)}
-
+        </Pressable >
+        {modalVisible ? (<Popup />) : (<View />)}
         <View style={styles.container}>
           <ActionSheet
             ref={actionSheet}
             title={'Baby muốn làm gì nào 🤔'}
             options={optionArray}
-            cancelButtonIndex={3}
-            destructiveButtonIndex={3}
+            cancelButtonIndex={2}
+            useNativeDriver={true}
+            destructiveButtonIndex={2}
             onPress={(index) => {
               if (index === 0) {
                 setModalVisible(true);
               } else if (index === 1) {
                 selectFile();
-              } else {
-                alert('ok')
               }
             }}
           />
         </View>
+
+
       </View>
       <View style={styles.headerText}>
-        <Text style={styles.appName}> Thảo Nguyễn Todo App </Text>
+        <Text style={styles.appName}> {appName} </Text>
         <Text style={styles.appSlogan}> Bắt đầu ngày mới thôi nào</Text>
         <View>
           <View style={styles.rowDirect}>
@@ -163,7 +194,9 @@ const Header = () => {
           </View>
         </View>
       </View>
+
     </View>
+
   );
 };
 
@@ -191,6 +224,11 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5
   },
+  changeAppNameInput: {
+    borderWidth: 1 / 2,
+    width: 200,
+    borderRadius: 15,
+  },
   button: {
     borderRadius: 20,
     padding: 10,
@@ -210,13 +248,16 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-    textAlign: 'center',
-    paddingTop: 30,
-    backgroundColor: '#307ecc',
-    padding: 16,
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignContent: 'center',
+    // textAlign: 'center',
+    // paddingTop: 30,
+    backgroundColor: 'red',
+    // marginLeft: 200
+    // padding: 16,
+    // borderWidth: 1,
+    // borderRadius: 15
   },
   buttonStyle: {
     width: '100%',
